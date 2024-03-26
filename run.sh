8d39a5f77dd7a8cfbfb23bd98f42bba03c27946f
racoot <sukhmanghum66778899@gmail.com>
2024-03-26 15:02:22 -0400

    updated: run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python main.py --n_nodes 1 --n_devices_per_node 1 --per_device_batch_size 2 --accumulate_grad_batches 1 --run_wandb
+python main.py --n_nodes 1 --n_devices_per_node 1 --run_wandb
